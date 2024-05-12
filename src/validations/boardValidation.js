94d48a5fdabdcd4a229d815a9de0c4afe3e1b33e
tanhtt <flywtan@gmail.com>
2024-05-12 11:35:01 +0700

    validation board

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,46 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+// https://joi.dev/api/?v=17.13.0
+
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+// Backend must be validata data
+// To custom message use .messages()
+
+const createNew = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title length must be at least 3 characters long',
+      'string.max': 'Title length must be less than or equal 50 characters long',
+      'string.trim': 'Title must not have leading or trail whitespace'
+    }),
+    description: Joi.string().required().min(3).max(250).trim().strict()
+  })
+
+  try {
+    console.log(req.body)
+
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+
+    // next()
+    res.status(StatusCodes.CREATED).json({ message: 'POST from validation: API create new board' })
+  } catch (error) {
+    console.log(error)
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+
+  
+}
+
+export const boardValidation = {
+  createNew
+}
