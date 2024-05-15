89d21e51732d2342e59dd7d7ea77cf78fc21202c
tanhtt <flywtan@gmail.com>
2024-05-16 02:46:09 +0700

    model layer board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -4,6 +4,7 @@
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
 import { slugify } from '~/utils/fommaters'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -13,9 +14,16 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
 
+    // Call to model to execute records new board
+    const createdBoard = await boardModel.createNew(newBoard)
+    console.log(createdBoard)
+
+    // Find created board record by id
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    console.log(getNewBoard)
 
     // In Service, always have to return data
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
